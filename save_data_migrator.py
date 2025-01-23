f9eeefc6411de09e4c0c91fbd87097058ccd0e2b
Toasted Lune <a@example.com>
2025-01-22 22:53:17 -0600

    Made save_data_migrator not rely on a function from main

--- save_data_migrator.py
+++ save_data_migrator.py
@@ -1,7 +1,6 @@
 import os
 import importlib
 import sys
-from main import file_contents_without_newlines
 def main():
     os.chdir("saves")
     sys.path.append(os.getcwd())
@@ -12,11 +11,20 @@ def main():
     all_save_data = importlib.import_module(f"{slot_to_migrate}_stored_save_data")
     with open(f"{slot_to_migrate}_stored_save_data.py") as save_file:
         with open(f"{slot_to_migrate}_save_data.py", "w+") as new_save_file:
-            if "active = True" in file_contents_without_newlines(save_file):
+            if "active = True" in file_contents_without_newlines(f"{slot_to_migrate}_stored_save_data.py"):
                 new_save_file.write(f"active = {all_save_data.active}\n")
             new_save_file.write(f"inventory = {all_save_data.inventory}\n")
             for data, value in all_save_data.save_data.items():
                 new_save_file.write(f"{data} = {value}\n")
     os.remove(f"{slot_to_migrate}_stored_save_data.py")
     print(f"Successfully migrated data, please note that the full name of the save file is {slot_to_migrate}_save_data.py")
+
+def file_contents_without_newlines(save_slot):
+    save_data_contents_without_newline = []
+    with open(save_slot) as save_data:
+        for line in save_data.readlines():
+            line = line.replace("\n", "")
+            save_data_contents_without_newline.append(line)
+        return save_data_contents_without_newline
+
 main()
